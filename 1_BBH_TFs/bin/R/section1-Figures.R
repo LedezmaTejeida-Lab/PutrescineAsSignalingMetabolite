091cd3f539e92315537f8489f2c7740acac633cf
emhernan <emhernan@lcg.unam.mx>
2024-01-15 17:09:22 -0600

    resizing figures

--- 1_BBH_TFs/bin/R/section1-Figures.R
+++ 1_BBH_TFs/bin/R/section1-Figures.R
@@ -31,12 +31,12 @@
 ###########################################################################
 
 
-dot_plot <- function(df, x_axe, y_axe, xilim, xslim, yilim, yslim, ylab, xlab, flagcolab = TRUE, colab = "", titlelab, xsize, ysize, textsize, titlesize = 12){
+dot_plot <- function(df, x_axe, y_axe, xilim, xslim, yilim, yslim, ylab, xlab, flagcolab = TRUE, colab = "", titlelab, xsize, ysize, textsize, titlesize = 12, legendsize = 7,angleV = 0){
   
   ggobject <- df %>% 
     ggplot(aes(x = x_axe, y = y_axe, color = x_axe)) +
     geom_point(position=position_jitter(h=0.1, w=0.1))
-  ggobject <- plot_settings(ggobject, xilim, xslim, yilim, yslim, ylab, xlab, flagcolab, colab, titlelab, xsize, ysize, textsize, titlesize)
+  ggobject <- plot_settings(ggobject, xilim, xslim, yilim, yslim, ylab, xlab, flagcolab, colab, titlelab, xsize, ysize, textsize, titlesize, legendsize, angleV)
   
   return(ggobject)
   
@@ -46,12 +46,12 @@ dot_plot <- function(df, x_axe, y_axe, xilim, xslim, yilim, yslim, ylab, xlab, f
 ########################## density_plot ###################################
 ###########################################################################
 
-density_plot <- function(df, x_axe, col, filler, xilim, xslim, yilim, yslim, ylab, xlab, flagcolab = FALSE, colab = "", titlelab, xsize, ysize, textsize, titlesize = 12){
+density_plot <- function(df, x_axe, col, filler, xilim, xslim, yilim, yslim, ylab, xlab, flagcolab = FALSE, colab = "", titlelab, xsize, ysize, textsize, titlesize = 12, legendsize = 7, angleV = 0){
   
   ggobject <- df %>%
     ggplot(aes(x = x_axe)) +
     geom_density(colour = col, fill = filler)
-  ggobject <- plot_settings(ggobject, xilim, xslim, yilim, yslim, ylab, xlab, flagcolab, colab, titlelab, xsize, ysize, textsize, titlesize)
+  ggobject <- plot_settings(ggobject, xilim, xslim, yilim, yslim, ylab, xlab, flagcolab, colab, titlelab, xsize, ysize, textsize, titlesize, legendsize, angleV)
   
   return(ggobject)
 }
@@ -60,7 +60,7 @@ density_plot <- function(df, x_axe, col, filler, xilim, xslim, yilim, yslim, yla
 ######################### plot_settings ###################################
 ###########################################################################
 
-plot_settings <- function(ggobj, xilim, xslim, yilim, yslim, ylab, xlab, flagcolab, colab, titlelab, xsize, ysize, textsize, titlesize){
+plot_settings <- function(ggobj, xilim, xslim, yilim, yslim, ylab, xlab, flagcolab, colab, titlelab, xsize, ysize, textsize, titlesize, legendsize, angleV){
   
   ggobj <- ggobj +
     coord_cartesian(ylim = c(yilim, yslim), xlim = c(xilim, xslim)) +
@@ -68,14 +68,13 @@ plot_settings <- function(ggobj, xilim, xslim, yilim, yslim, ylab, xlab, flagcol
     labs(y = ylab, 
          x = xlab,
          title = titlelab) +
-    theme(plot.title = element_text(hjust = 0.5, size = titlesize, face = "italic"), 
+    theme(legend.title=element_text(size=legendsize),
+          legend.text = element_text(size = legendsize),
+          plot.title = element_text(hjust = 0.5, size = titlesize, face = "italic"), 
           axis.title.x = element_text(size = xsize),
           axis.title.y = element_text(size = ysize),
           axis.text = element_text(size = textsize),
-          panel.background = element_rect(fill='transparent'),
-          plot.background = element_rect(fill='transparent', color=NA),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank()) +
+          axis.text.x = element_text(angle = angleV, hjust = 1)) +
     scale_colour_viridis(option="mako", direction = -1) 
   
   if(flagcolab){
@@ -132,37 +131,35 @@ print("........................Processing data ........................")
 dotplotEcoli <- dot_plot(df = ecoli, x_axe = ecoli$peri, y_axe = ecoli$coveragePercent, 
                       xilim = 0, xslim = 100, yilim = 0, yslim = 100, ylab = "Query Coverage (%)", 
                        xlab = "Identity (%)", colab = 'Identity (%)', 
-                       titlelab = "Escherichia coli\n", xsize = 10, ysize = 10, textsize = 8, titlesize = 12)
+                       titlelab = "Escherichia coli\n", xsize = 8.5, ysize = 8.5, textsize = 8.5, titlesize = 8.5, legendsize = 8)
 
 dotplotRphaseoli <- dot_plot(df = rphaseoli, x_axe = rphaseoli$peri, y_axe = rphaseoli$coveragePercent, 
                           xilim = 0, xslim = 100, yilim = 0, yslim = 100, ylab = "Query Coverage (%)", 
                           xlab = "Identity (%)", colab = 'Identity (%)', 
-                          titlelab = "Rhizobium phaseoli\n", xsize = 10, ysize = 10, textsize = 8, titlesize = 12)
+                          titlelab = "Rhizobium phaseoli\n", xsize = 8, ysize = 8, textsize = 8, titlesize = 8.5, legendsize = 8)
 
 
 ecoliEvalue <- density_plot(df = ecoli, x_axe = ecoli$Evalue, col = "#e2127f", filler = "#fbd1e2", 
              xilim = 0, xslim =  max(ecoli$Evalue), yilim = 0, yslim = 3.5e+23, ylab = "Density", xlab = "E-value", 
-           titlelab = "Escherichia coli", xsize = 8, ysize = 8, textsize = 6, titlesize = 9)
+           titlelab = "Escherichia coli", xsize = 7.5, ysize = 7.5, textsize = 6, titlesize = 8, angleV = 45)
 
 rphaseoliEvalue <- density_plot(df = rphaseoli, x_axe = rphaseoli$Evalue, col = "#e2127f", filler = "#fbd1e2", 
                                  xilim = 0, xslim =  max(rphaseoli$Evalue), yilim = 0, yslim = 14e+23, ylab = "Density", xlab = "E-value", 
-                                 titlelab = "Rhizobium phaseoli", xsize = 8, ysize = 8, textsize = 6, titlesize = 9)
+                                 titlelab = "Rhizobium phaseoli", xsize = 7.5, ysize = 7.5, textsize = 6, titlesize = 8, angleV = 45)
 
 ecoliQlen <-  density_plot(df = ecoli, x_axe = ecoli$qlen, col = "#10c3a6", filler = "#93f5e5", 
                             xilim = 0, xslim = 1300, yilim = 0, yslim = 0.004, ylab = "Density", xlab = "Query length (bp)", 
-                            titlelab = "Escherichia coli", xsize = 8, ysize = 8, textsize = 6, titlesize = 9)
+                            titlelab = "Escherichia coli", xsize = 7.5, ysize = 7.5, textsize = 6, titlesize = 8)
 
 rphaseoliQlen <-  density_plot(df = rphaseoli, x_axe = rphaseoli$qlen, col = "#10c3a6", filler = "#93f5e5", 
                             xilim = 0, xslim =  1300, yilim = 0, yslim = 0.004, ylab = "Density", xlab = "Query length (bp)", 
-                            titlelab = "Rhizobium phaseoli", xsize = 8, ysize = 8, textsize = 6, titlesize = 9)
+                            titlelab = "Rhizobium phaseoli", xsize = 7.5, ysize = 7.5, textsize = 6, titlesize = 8)
 
 
 MainF1 <- ggarrange(dotplotEcoli, dotplotRphaseoli, 
                           labels = c("A", "B"), 
                           ncol = 2, nrow = 1, 
-                          common.legend = TRUE, legend="bottom") +
-		  theme(legend.background = element_rect(fill='transparent'),
-          legend.box.background = element_rect(fill='transparent'))
+                          common.legend = TRUE, legend="bottom")
 
 SuppF1 <- ggarrange(ecoliEvalue, rphaseoliEvalue, ecoliQlen, rphaseoliQlen,
                      labels = c("A", "B", "C", "D"), 
@@ -171,22 +168,11 @@ SuppF1 <- ggarrange(ecoliEvalue, rphaseoliEvalue, ecoliQlen, rphaseoliQlen,
 print("........................Saving data ........................")
 
 pngpath <- paste(outpath, "MainF1.png",sep="")
-png(pngpath, width=5.59, height=6.2, res=300, units="in", bg = "transparent")
+png(pngpath, width=3*300, height=4.25*300, res=300)
 MainF1
 dev.off()
 
 pngpath <- paste(outpath, "SuppF2.png",sep="")
-png(pngpath, width=4.5, height=5.5, res=300, units="in", bg = "transparent")
+png(pngpath, width=3*300, height=4.25*300, res=300)
 SuppF1
 dev.off()
-
-pdfpath <- paste(outpath, "MainF1.pdf",sep="")
-pdf(pdfpath, width=5.59, height=6.2, pointsize=300, bg = "transparent")
-MainF1
-dev.off()
-
-pdfpath <- paste(outpath, "SuppF2.pdf",sep="")
-pdf(pdfpath, width=4.5, height=5.5, pointsize=300, bg = "transparent")
-SuppF1
-dev.off()
-
